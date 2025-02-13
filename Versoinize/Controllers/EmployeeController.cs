45ddefcd196e3be8bf405510517cbf64b0703b3e
a.mohsen <a.mohsen@asis-it.com>
2025-02-13 10:21:43 +0200

    Add project files.

---
+++ Versoinize/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Versoinize.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("EmployeeController");
+        }
+    }
+}
---
+++ Versoinize/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Versoinize.Models
+{
+    public class Employee
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+
+      
+    }
+}
